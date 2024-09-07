7f4f853d8245a0d9afb0962bd8c99f48ba108128
Parag Gupta <paraggupta9988@gmail.com>
2024-09-08 03:47:20 +0530

    Add RewardValue class for cash-to-miles conversion
    - Implemented RewardValue class with a static cashValue field.
    - Added a constant MILES_CONVERSION_RATE for conversion calculations.
    - Created a constructor to initialize cashValue.
    - Added a method getMilesValue() to convert cash to miles.

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -18,3 +18,17 @@ public class RewardsConverter {
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
 }
+
+class RewardValue{
+    private static double cashValue = 0;
+    private static final double  MILES_CONVERSION_RATE = 0.0035;
+    // Constructor
+    public RewardValue(double cashValue) {
+        RewardValue.cashValue = cashValue;
+    }
+
+    // Method to convert cash to miles
+    public double getMilesValue() {
+        return cashValue / MILES_CONVERSION_RATE;
+    }
+}
